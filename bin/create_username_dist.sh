c0a6d36fbfe48b5ea604bb55f1feecf2eec32b11
crame160 <dylancramer@protonmail.com>
2021-09-14 17:27:47 -0500

    Started work on create_username_dist. It's currently entirely functional except for the creation of the actual file. Touched up process_client_logs to make it pass shellcheck and massively improved the regex.

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
+
+# cd "$1" || return
+
+for file in $1/*/
+do
+awk '{print $4}' "$file/failed_login_data.txt" | \
+
+	sort | \
+
+	uniq -c | \
+
+	sort -nr | \
+       
+	awk '{print "data.addRow([\x27" $2 "\x27, " $1 ");"}' | \
+
+	./wrap_contents.sh "html_components/username_dist" "username_dist.html"
+done
+
+
 

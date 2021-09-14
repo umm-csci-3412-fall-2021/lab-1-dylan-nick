0d158c629ca73fd0f6538db2bea3a2e75b5b447b
crame160 <dylancramer@protonmail.com>
2021-09-14 12:06:44 -0500

    Another attempt at wrangling with git and making it push these changes properly

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+cd $1
 
 for file in var/log/*
 do
-	awk 'match($0, /(.*Failed password for ([a-zA-Z]+ )+([0-9]+\.)+[0-9]+)/, groups) {print groups[1]}' $file
+	#               Capture the month, date and the hours of the time.         check for 0 or 1 invalid user, capture username, and then capture the IP
+	awk 'match($0, /(([a-zA-Z]+[[:space:]]+[0-9]+ [0-9]+).*Failed password for (invalid user )?([[:alnum:]]+) from ([0-9\.]+))/, groups) {printf groups[2] " " groups[4] " "  groups[5] "\n"}' $file >> failed_login_data.txt
 done

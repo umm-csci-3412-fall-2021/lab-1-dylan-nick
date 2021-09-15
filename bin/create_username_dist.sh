4f4d97ec0b7bc3dd7e41a4ebac170952b46923aa
crame160 <dylancramer@protonmail.com>
2021-09-14 21:33:56 -0500

    Completed create_username_dist.sh and commented on every line of the bat file. Passes shellcheck. Passes bats tests.

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-# cd "$1" || return
-
-for file in $1/*/
+# Iterate through every folder in the given data directory
+for file in "$1"/*/
 do
-awk '{print $4}' "$file/failed_login_data.txt" | \
-
-	sort | \
-
-	uniq -c | \
-
-	sort -nr | \
-       
-	awk '{print "data.addRow([\x27" $2 "\x27, " $1 ");"}' | \
-
-	./wrap_contents.sh "html_components/username_dist" "username_dist.html"
+	# Add all of our data to a temporary txt file since we need ALL of the data before we can begin sorting
+	awk '{print $4}' "$file/failed_login_data.txt" >> allnames.txt
 done
 
+# Sort the txt so uniq has the proper input, sort alphabetally
+sort allnames.txt | \
 
+	# count the number of each unique entry
+	uniq -c | \
 
+	# use awk to imbed the values of each line into a new row of javascript and output it
+	# to a new temporary txt file. 	
+	awk '{print "data.addRow([\x27" $2 "\x27, " $1 "]);"}' >> output.txt
+
+# Finally, wrap the contents of that output with the porper header and footer
+./bin/wrap_contents.sh "output.txt" "html_components/username_dist" "$1/username_dist.html"
+
+# Cleanup
+rm output.txt
+rm allnames.txt

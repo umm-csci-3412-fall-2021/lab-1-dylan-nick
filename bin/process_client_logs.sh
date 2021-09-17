252c7f6b77f3900733ae8003ec8f4759c1069b3d
crame160 <dylancramer@protonmail.com>
2021-09-17 02:02:31 -0500

    Fixed the regex, hopefully this one last cursed time. This should finally pass the process logs github bats test. Every group is now individually captured so there is absolutely no extra whitespace.

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -2,6 +2,7 @@
 
 for file in "$1"/var/log/*
 do
-	#               Capture the month, date and the hours of the time.         check for 0 or 1 invalid user, capture username, and then capture the IP
-	awk 'match($0, /(([a-zA-Z]+[[:space:]]+[0-9]+ [0-9]+).*Failed password for (invalid user )?([[:alnum:]]+) from ([0-9\.]+))/, groups) {printf groups[2] " " groups[4] " "  groups[5] "\n"}' "$file" >> "$1"/failed_login_data.txt
+	#       Capture the month,               date     time.        check for 0 or 1 invalid user, capture username, and then IP.            Print every last one of them, 1 by 1 so there is absolutely NO EXTRA WHITESPACE
+        awk 'match($0, /([a-zA-Z]+)[[:space:]]+([0-9]+) ([0-9]+).*Failed password for (invalid user )?([[:alnum:]]+) from ([0-9\.]+)/, groups) {printf groups[1] " " groups[2] " "  groups[3] " " groups[5] " " groups[6] "\n"}' "$file" >> "$1"/failed_login_data.txt
+
 done

dc1f2990d89860edfac733733835667394de51b4
crame160 <dylancramer@protonmail.com>
2021-09-17 04:45:47 -0500

    Finished process_logs.sh entirely and it passes all the bats tests! It also passes shellcheck. In the process of making it pass the bats test, I found one error in our regex what was making them fail from a few crafty crafty crafty names thrown in there to mess it up. Fixed that and tweaked process_client_logs.sh. failed_login_summary added for the heck of it.

--- bin/process_logs.sh
+++ bin/process_logs.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
 
+#Save working directory for output and create our temporary dir
+here=$(pwd)
+temp_dir="/tmp/tmp_logs"
+mkdir "$temp_dir"
+
+
+# Loop through every argument
 for arg
 do
-echo $arg
+	# Define the directory that we're going to extract the supplied tgz into and then create it
+	working_dir="$temp_dir"/$(basename -s _secure.tgz "$arg")
+	mkdir "$working_dir"
+
+	# Extract the log file into our working dir
+	tar zxf "$arg" --directory "$working_dir"
+
+	# Process those logs once we've extracted them using our working dir
+	./bin/process_client_logs.sh "$working_dir"
 done
+
+# Run all of our shell scripts to aggregate and assemble the data
+./bin/create_hours_dist.sh "$temp_dir"
+./bin/create_country_dist.sh "$temp_dir"
+./bin/create_username_dist.sh "$temp_dir"
+./bin/assemble_report.sh "$temp_dir"
+
+# Finally, move the summary to our current working directory we saved about!
+mv $temp_dir/failed_login_summary.html "$here"
+
+# Clean.
+rm -rf $temp_dir
